be2b0da16c5990fd63db6f79e3462eb1521265e4
Colleen Gilhuly <colleen.gilhuly@gmail.com>
2020-09-13 19:10:02 -0400

    Cleaned up loading of multiple CSVs

--- heatweek.py
+++ heatweek.py
@@ -17,8 +17,24 @@ def get_time_index(time, bin_width=15.0):
     return minutes / bin_width
 
 
-# Filters dataframe of parsed entries 
-# Keeps (or rejects) all rows with `colname` matching `keys` 
+# Load multiple CSVs exported from Toggl into one dataframe
+def read_CSVs(file_list):
+
+    df_list = []
+
+    for filename in file_list:
+        df_list.append(
+            pd.read_csv(
+                filename,
+                parse_dates=[["Start date", "Start time"], ["End date", "End time"]],
+            )
+        )
+
+    return pd.concat(df_list)
+
+
+# Filters dataframe of parsed entries
+# Keeps (or rejects) all rows with `colname` matching `keys`
 def filter_entries(entries, colname, keys, keep_keys=True):
 
     if keep_keys:
@@ -30,6 +46,10 @@ def filter_entries(entries, colname, keys, keep_keys=True):
         filtered = entries[indices]
 
     return filtered
+
+
+# Filters dataframe of parsed entries based on date
+#!# def filter_entries_bydate(entries, start_date, end_date):
 
 
 # Add one time entry to the week (2D-) histogram
@@ -55,6 +75,7 @@ def add_entry_to_week(week, start, end, bin_width=15.0):
         #!# Double yikes: if time entry spans multiple weeks
         #!# Safest approach is probably date-by-date approach
         print("Heck to working over midnight")  #!#
+        print(start, end)
 
     # Special case for time entries within one time bin
     elif int(start_index) == int(end_index):
@@ -83,44 +104,60 @@ def add_entry_to_week(week, start, end, bin_width=15.0):
 #!# Currently assumes time bins of 15 min
 def plot_week(week, save_plot=False):
 
-    fig, ax = plt.subplots(); 
-    plt.imshow(week, aspect = 0.05)
+    fig, ax = plt.subplots()
+    plt.imshow(week, aspect=0.05)
 
-    ax.set_xticks(np.arange(0,7,1))
-    ax.set_xticklabels(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+    ax.set_xticks(np.arange(0, 7, 1))
+    ax.set_xticklabels(
+        ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    )
     ax.set_yticks(np.arange(-0.5, 92, 4))
-    ax.set_yticklabels(["12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", 
-                        "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", 
-                        "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"])
+    ax.set_yticklabels(
+        [
+            "12 AM",
+            "1 AM",
+            "2 AM",
+            "3 AM",
+            "4 AM",
+            "5 AM",
+            "6 AM",
+            "7 AM",
+            "8 AM",
+            "9 AM",
+            "10 AM",
+            "11 AM",
+            "12 PM",
+            "1 PM",
+            "2 PM",
+            "3 PM",
+            "4 PM",
+            "5 PM",
+            "6 PM",
+            "7 PM",
+            "8 PM",
+            "9 PM",
+            "10 PM",
+            "11 PM",
+        ]
+    )
 
     # Setting up grid along (invisible) minor ticks
     ax.set_xticks(np.arange(-0.5, 6, 1), minor=True)
     ax.set_yticks(np.arange(11.5, 95.5, 12), minor=True)
-    ax.tick_params(which='minor', length=0)
-    ax.grid(which='minor', color='w', linewidth=1)
+    ax.tick_params(which="minor", length=0)
+    ax.grid(which="minor", color="w", linewidth=1)
 
     plt.tight_layout()
 
     if save_plot:
-        fig.savefig(save_plot, figsize=(14,11))
+        fig.savefig(save_plot, figsize=(14, 11))
 
     else:
         plt.show()
 
-# Read in tables of Toggl time entries
-entries_raw = pd.read_csv(
-    "Toggl_time_entries_2020-01-01_to_2020-12-31.csv",
-    parse_dates=[["Start date", "Start time"], ["End date", "End time"]],
-)
 
-entries_raw2 = pd.read_csv(
-    "Toggl_time_entries_2019-01-01_to_2019-12-31.csv",
-    parse_dates=[["Start date", "Start time"], ["End date", "End time"]],
-)
-
-# Combine data from both files into one dataframe
-#!# Is there a better way to do this with read_csv()?
-entries = entries_raw.append(entries_raw2)
+# Read time entries exported from Toggl into a dataframe
+entries = read_CSVs(["Toggl_entries_2020.csv", "Toggl_entries_2019.csv"])
 
 #!# Could make resolution of final chart a user-defined variable?
 #!# Would need to check that it divides well into 24 hours, and move to more granular underlying histogram
@@ -128,22 +165,29 @@ week = np.zeros((96, 7))
 
 for index, row in entries.iterrows():
 
-    #!# Could add condition based on Project, Tags, etc
     week = add_entry_to_week(
         week, row["Start date_Start time"], row["End date_End time"]
     )  #!# Ugly names
 
-plot_week(week, save_plot='test_plot_0.png')
+plot_week(week, save_plot="test_plot_0.png")
 
 
 # Filtering dataframe based on project name
-# Here I'm picking out all of my work-related projects 
+# Here I'm picking out all of my work-related projects
 # Other useful rows could be client or tags
-keys = ["Department service / volunteering",
-        "Meetings", "Online courses", "Other work",
-        "Productivity", "Professional development",
-        "Projects", "Research", "TAing", "Talks and seminars",
-        "Thesis"]
+keys = [
+    "Department service / volunteering",
+    "Meetings",
+    "Online courses",
+    "Other work",
+    "Productivity",
+    "Professional development",
+    "Projects",
+    "Research",
+    "TAing",
+    "Talks and seminars",
+    "Thesis",
+]
 
 work_entries = filter_entries(entries, "Project", keys)
 
@@ -156,4 +200,4 @@ for index, row in work_entries.iterrows():
         work_week, row["Start date_Start time"], row["End date_End time"]
     )  #!# Ugly names
 
-plot_week(work_week, save_plot='test_plot_1.png')
+plot_week(work_week, save_plot="test_plot_1.png")
